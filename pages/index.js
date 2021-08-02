d774bc4b6d73a7e22c784d867bf2729659a44b8b
Szymon <szymonzalarski98@gmail.com>
2021-08-02 20:30:18 +0200

    feat: google analitycs integration

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,23 @@ export default function Home() {
       <Head>
           <title>Wiktoria Grzywa</title>
           <link rel='icon' href='/favicon.ico' />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
       <NextSeo
           title="Wiktoria Grzywa Stylizacja Paznokci"
