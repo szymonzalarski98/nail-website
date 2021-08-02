c5bf92adc592c2fbdef4373171e5de8b05ee918e
Szymon <szymonzalarski98@gmail.com>
2021-08-02 20:15:49 +0200

    feat: add robots, sitemap files and integrate with GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "tailwindcss/tailwind.css";
+import { useEffect } from "react";
 import { Navbar } from "../components/Navbar";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
+  const gtmId = "GTM-K6J97XJ";
+  useEffect(() => {
+    TagManager.initialize({ gtmId });
+  }, []);
   return (
     <div className="bg-white">
       <Navbar />
